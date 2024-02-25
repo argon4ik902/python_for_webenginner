018b26be30408326e20f3e6d3c6914718bc05dd5
argon4ik902 <gryazevanton333@yandex.ru>
2024-02-25 23:23:34 +0400

    Задания до 4.5

--- exercises/04_data_structures/task_4_5.py
+++ exercises/04_data_structures/task_4_5.py
@@ -22,3 +22,10 @@
 
 command1 = "switchport trunk allowed vlan 1,2,3,5,8"
 command2 = "switchport trunk allowed vlan 1,3,8,9"
+
+command1 = command1.split()
+command1 = command1[-1].split(',')
+command2 = command2.split()
+command2 = command2[-1].split(',')
+
+
